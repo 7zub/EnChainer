e216a59da3d6e84d6e94d6b5348067bd2c7dd5e2
Samir <7zub@bk.ru>
2024-09-26 22:53:36 +0300

    processing multiple exchanges

--- models/Request.go
+++ models/Request.go
@@ -1,6 +1,13 @@
 package models
 
-import "awesomeProject/controllers"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"reflect"
+	"strings"
+)
 
 type IParams interface {
 	GetParams(ccy string) Request
@@ -14,5 +21,36 @@ type Request struct {
 }
 
 func (r Request) SendRequest() {
-	controllers.ApiGetBook1(r)
+	r.UrlCreator()
+}
+
+func (r Request) UrlCreator() {
+	fields := reflect.TypeOf(r.Params)
+	values := reflect.ValueOf(r.Params)
+
+	rq, err := http.NewRequest("GET", r.Url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	q := rq.URL.Query()
+
+	for i := 0; i < fields.NumField(); i++ {
+		fmt.Println(fields.Field(i).Name)
+		fmt.Println(fmt.Sprintf("%v", values.Field(i)))
+		//fmt.Println(values.FieldByIndex([]int{i}).String())
+		q.Add(strings.ToLower(fields.Field(i).Name), fmt.Sprintf("%v", values.Field(i)))
+	}
+
+	rq.URL.RawQuery = q.Encode()
+
+	fmt.Printf("Полный URL: %s\n", rq.URL.String())
+	client := http.Client{}
+	resp, err := client.Do(rq)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+	log.Println(result)
 }
